8ad5db371485d0f4053efd1556b4a6f4f01f830b
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2025-09-26 13:40:05 +0700

    Config yml (dev,test,prod) and yml common use profile active

---
+++ NextStepBackend/src/main/java/org/example/nextstepbackend/NextStepBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.nextstepbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextStepBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextStepBackendApplication.class, args);
+    }
+
+}
---
+++ NextStepBackend/src/test/java/org/example/nextstepbackend/NextStepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nextstepbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
