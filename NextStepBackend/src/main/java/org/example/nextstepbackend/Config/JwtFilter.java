f80212f7b74b96060040a24ba7a425fa81d8da67
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2025-11-25 00:43:47 +0700

    fix warn jwtfilter

--- NextStepBackend/src/main/java/org/example/nextstepbackend/Config/JwtFilter.java
+++ NextStepBackend/src/main/java/org/example/nextstepbackend/Config/JwtFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.example.nextstepbackend.Utils.JwtUtil;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,7 +24,11 @@ public class JwtFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        // JWT filter: validate token and set authentication if valid
         String header = request.getHeader("Authorization");
         if (header == null || header.isBlank() || !header.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
