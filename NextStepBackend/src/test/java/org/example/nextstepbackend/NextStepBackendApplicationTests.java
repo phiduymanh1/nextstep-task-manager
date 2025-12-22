6798ea05ded5aa9bb96ff9842aceb367129debb1
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2025-12-22 23:39:30 +0700

    fix bug scope test

--- NextStepBackend/src/test/java/org/example/nextstepbackend/NextStepBackendApplicationTests.java
+++ NextStepBackend/src/test/java/org/example/nextstepbackend/NextStepBackendApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.nextstepbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NextStepBackendApplicationTests {
 
   @Test
