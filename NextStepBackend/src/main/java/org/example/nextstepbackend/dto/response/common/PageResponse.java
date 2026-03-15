0afbabe512fe141f70513996c9e9454b8fa50066
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2026-03-15 15:25:20 +0700

    Response page response

---
+++ NextStepBackend/src/main/java/org/example/nextstepbackend/dto/response/common/PageResponse.java
@@ -0,0 +1,11 @@
+package org.example.nextstepbackend.dto.response.common;
+
+import java.util.List;
+
+public record PageResponse<T>(
+        List<T> items,
+        int page,
+        int size,
+        long totalElements,
+        int totalPages
+) {}
