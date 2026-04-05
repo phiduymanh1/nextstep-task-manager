8bc61e6c5f0ad6424111bdfcaf0eac5ab390c465
PHI_MANH\manhp <phiduymanh.dev@gmail.com>
2026-04-05 15:05:11 +0700

    feat: fe call api toggleChecklistItem

--- NextStepFrontend/src/services/checklist.service.ts
+++ NextStepFrontend/src/services/checklist.service.ts
@@ -1,5 +1,7 @@
 import api from '@/api';
 
+const baseUserUrl = '/checklists';
+
 export interface ChecklistRequest {
   title: string;
   afterId?: number | null;
@@ -22,7 +24,7 @@ export interface ChecklistResponse {
 export interface ChecklistItemRequest {
   content: string;
   position?: number;
-  dueDate?: string | null; 
+  dueDate?: string | null;
   afterId?: number | null;
   beforeId?: number | null;
 }
@@ -41,17 +43,20 @@ export const createChecklist = async (
   cardId: number,
   data: ChecklistRequest
 ): Promise<ChecklistResponse> => {
-  const res = await api.post(`/checklists/${cardId}`, data);
+  const res = await api.post(`${baseUserUrl}/${cardId}`, data);
   return res.data.data;
 };
-
 
 export const createChecklistItem = async (
   checklistId: number,
   data: ChecklistItemRequest
 ): Promise<ChecklistItemResponse> => {
-  const res = await api.post(`/checklists/${checklistId}/items`, data);
+  const res = await api.post(`${baseUserUrl}/${checklistId}/items`, data);
 
   return res.data.data;
 };
 
+export const toggleChecklistItem = async (checklistItemId: number) => {
+  const res = await api.post(`${baseUserUrl}/${checklistItemId}/toggle`);
+  return res.data;
+};
